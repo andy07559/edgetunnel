4a5b698c5526a9351b23aa4d361ab0d968ba95e1
andy07559 <32185128+andy07559@users.noreply.github.com>
2024-12-10 20:13:12 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,16 +1,16 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '37f106ae-10a1-4de1-b5b8-23907fdee290';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
 let subProtocol = 'https';
 let subEmoji = 'true';
-let socks5Address = '';
+let socks5Address = 'e07559:Cs123543@s5.8p.ink:58035';
 let parsedSocks5Address = {}; 
-let enableSocks = false;
+let enableSocks = true;
 
 let fakeUserID ;
 let fakeHostName ;
@@ -25,7 +25,7 @@ let go2Socks5s = [
 	'*.loadshare.org',
 ];
 let addresses = [];
-let addressesapi = [];
+let addressesapi = [https://c.139.ink/f/JanSO/s13.txt];
 let addressesnotls = [];
 let addressesnotlsapi = [];
 let addressescsv = [];
